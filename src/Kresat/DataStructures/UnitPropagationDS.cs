b2bbd5d7d3f915119e35df358c89cfecf6bcaf2b
Kristyna Petrlikova <petrlik@matfyz.cz>
2024-07-26 16:56:39 +0200

    data structures: unify unit propagation

---
+++ src/Kresat/DataStructures/UnitPropagationDS.cs
@@ -0,0 +1,154 @@
+namespace Kresat.Representations {  
+    enum Valuation {
+        FALSIFIED,
+        UNSATISFIED,
+        SATISFIED
+    }
+    internal record struct Decision {
+        public int DecisionLevel { get; internal set; }
+        public int Literal { get; internal set; }
+    }
+    interface ICreateFromLiterals<TClause, TLiteral> {
+        static abstract TClause Create(List<int> literals, List<TLiteral> literalData);
+    }
+    interface ICreateFromLiteralData<TLiteral> {
+        static abstract TLiteral Create(List<TLiteral> literalData);
+    }
+    interface IClause<TLiteral> where TLiteral : ILiteral<TLiteral> {
+        public List<int> Literals {get;set;}
+        public List<TLiteral> literalData {get;set;}
+        bool IsUnit();
+        bool IsFalsified();
+        int GetUnitLiteral();
+    }
+    interface ILiteral<TLiteral> where TLiteral : ILiteral<TLiteral> {
+        public Valuation Value {get;}
+        public List<TLiteral> literalData {get;set;}
+        void Satisfy();
+        void Falsify();
+        void Unsatisfy();
+        IEnumerable<IClause<TLiteral>> GetClauses();
+    }
+    internal static class ListExtensions {
+        public static void Swap<T>(this IList<T> list, int idx1, int idx2){
+            if(idx1 == idx2) return;
+            T tmp = list[idx1];
+            list[idx1] = list[idx2];
+            list[idx2] = tmp;
+        }
+        public static void RemoveInPlace<T>(this IList<T> list, int idx){
+            list.Swap(idx, list.Count-1);
+            list.RemoveAt(list.Count-1);
+        }
+        public static TLiteral At<TLiteral>(this IList<TLiteral> list, int lit){
+            if (lit < 0) {
+                return list[-2*(lit + 1)];
+            }
+            return list[2*(lit - 1) + 1];
+        }
+    }
+    abstract class UnitPropagationDS<TLiteral, TClause> where TLiteral : ILiteral<TLiteral>, ICreateFromLiteralData<TLiteral>
+                                                        where TClause : IClause<TLiteral>, ICreateFromLiterals<TClause, TLiteral> {
+        public bool HasContradiction = false;
+        public int currDecisionLevel = 0;
+        public int unitPropSteps = 0;
+        internal record struct Decision {
+            public int DecisionLevel { get; internal set; }
+            public int Literal { get; internal set; }
+        }
+        internal Stack<Decision> decisions = new();
+        List<TLiteral> literalData;
+        List<TClause> clauseData;
+
+        internal int ChooseDecisionLiteral(){
+            return UndecidedVars.First();
+        }
+        HashSet<int> UndecidedVars = new();
+        Stack<TClause> unitClauses = new();
+
+        public void Backtrack(int decisionLevel){
+            HasContradiction = false;
+            unitClauses.Clear();
+            while(decisions.Count > 0 && decisions.Peek().DecisionLevel > decisionLevel){
+                Decision decision = decisions.Pop();
+                UndoLiteral(decision.Literal);
+            }
+            currDecisionLevel = decisionLevel;
+        }
+
+        private void UndoLiteral(int literal){
+            UndecidedVars.Add(Math.Abs(literal));
+            literalData.At(literal).Unsatisfy();;
+            literalData.At(-literal).Unsatisfy();
+        }
+        public void DecideLiteral(int literal){
+            currDecisionLevel++;
+            AssignLiteral(literal);
+        }
+
+        public void UndoLastLiteral(){
+            Backtrack(currDecisionLevel-1);
+        }
+
+        internal List<int> ConstructModel()
+        {
+            List<int> res = new List<int>();
+            for(int _var = 1; _var <= literalData.Count / 2; _var++){
+                if(literalData.At(_var).Value == Valuation.UNSATISFIED){
+                    throw new ArgumentException();
+                }
+                res.Add(literalData.At(_var).Value == Valuation.SATISFIED ? _var : -_var);
+            }
+            return res;
+        }
+
+        protected void AddClause(List<int> literals, List<TLiteral> literalData){
+            TClause clause = TClause.Create(literals, literalData);
+            clauseData.Add(clause);
+            if(clause.IsUnit()){
+                unitClauses.Push(clause);
+            }
+        }
+
+        protected void AssignLiteral(int literal){
+            decisions.Push(new Decision { DecisionLevel = currDecisionLevel, Literal = literal });
+            UndecidedVars.Remove(Math.Abs(literal));
+            literalData.At(literal).Satisfy();
+            literalData.At(-literal).Falsify();
+            foreach(TClause clause in literalData.At(-literal).GetClauses()){
+                if(clause.IsUnit()){
+                    unitClauses.Push(clause);
+                }
+                if(clause.IsFalsified()){
+                    HasContradiction = true;
+                }
+            }
+        }
+
+        public UnitPropagationDS(CommonRepresentation cr){
+            literalData = new List<TLiteral>( new TLiteral[2*cr.LiteralCount]);
+            for(int i = 0; i < literalData.Count; i++){
+                literalData[i] = TLiteral.Create(literalData);
+            }
+            clauseData = new();
+            UndecidedVars = new HashSet<int>(Enumerable.Range(1, cr.LiteralCount));
+            foreach(var literals in cr.Clauses){
+                AddClause(literals, literalData);
+            }
+        }
+        public void UnitPropagation(){
+            while(unitClauses.Count > 0 && !HasContradiction){
+                unitPropSteps++;
+                TClause currClause = unitClauses.Pop();
+                if(!currClause.IsUnit()){
+                    if(currClause.IsFalsified()){
+                        HasContradiction = true;
+                    }
+                    continue;
+                }
+                int lit = currClause.GetUnitLiteral();
+                AssignLiteral(lit);
+            }      
+        }
+    }   
+}
--- src/Kresat/DataStructures/WatchedLiterals.cs
+++ src/Kresat/DataStructures/WatchedLiterals.cs
@@ -8,71 +8,16 @@
 */
 
 namespace Kresat.Representations {
-  static class ListExtensions {
-    public static void Swap<T>(this IList<T> list, int idx1, int idx2){
-      if(idx1 == idx2) return;
-      T tmp = list[idx1];
-      list[idx1] = list[idx2];
-      list[idx2] = tmp;
-    }
-    public static void RemoveInPlace<T>(this IList<T> list, int idx){
-      list.Swap(idx, list.Count-1);
-      list.RemoveAt(list.Count-1);
-    }
-  }
-  class WatchedLiterals {
-    public bool HasContradiction = false;
-    public int currDecisionLevel = 0;
-    public int unitPropSteps = 0;
-    internal record struct Decision {
-      public int DecisionLevel { get; internal set; }
-      public int Literal { get; internal set; }
-    }
-    internal Stack<Decision> decisions = new();
-
-    static int LiteralIdx(int lit){
-      if (lit < 0) {
-        return -2*(lit + 1);
-      }
-      return 2*(lit - 1) + 1;
-    }
-    HashSet<int> UndecidedVars = new();
-    Stack<Clause> unitClauses = new();
-
-    class Literal {
-      public Valuation Value = Valuation.UNSATISFIED;
-      public List<Watch> Watches = new();
-      public required List<Literal> literalData {get; init;}
-      public void Satisfy(){
-        Value = Valuation.SATISFIED;
-      }
-      public void Falsify(){
-        Value = Valuation.FALSIFIED;
-        for(int i = 0; i < Watches.Count; i++){
-          //Console.WriteLine($"before falsification: {Watches[i].LitPos}");
-          int prev = Watches[i].LitPos;
-          if(Watches[i].FindNext()){
-            int newLit = Watches[i].clause.Literals[Watches[i].LitPos];
-            if(prev == Watches[i].LitPos){
-              throw new ArgumentException("aaa");
-            }
-            //Console.WriteLine($"after falsification: {Watches[i].LitPos}");
-            literalData[LiteralIdx(newLit)].Watches.Add(Watches[i]);
-            Watches.RemoveInPlace(i);
-            i--;
-          }
-        }
-      }
-      public void Unsatisfy(){
-        Value = Valuation.UNSATISFIED;
-      }
-    }
-    class Clause {
+    internal class WatchClause : IClause<WatchLiteral>,
+                                 ICreateFromLiterals<WatchClause, WatchLiteral> {
       public Watch? w1 {get; private set;}
       public Watch? w2 {get; private set;}
-      public List<int> Literals {get;private set;}
-      List<Literal> literalData;
-      public Clause(List<int> literals, List<Literal> literalData){
+      public List<int> Literals {get;set;}
+      public List<WatchLiteral> literalData {get;set;}
+      public static WatchClause Create(List<int> literals, List<WatchLiteral> literalData){
+        return new WatchClause(literals, literalData);
+      }
+      public WatchClause(List<int> literals, List<WatchLiteral> literalData){
         this.Literals = literals;
         this.literalData = literalData;
         ConstructWatches();
@@ -85,29 +30,43 @@ namespace Kresat.Representations {
         w2 = new Watch(LitPos: 1, clause: this);
         w1.FindNext();
         w2.FindNext();
-        literalData[LiteralIdx(GetLiteralForWatch(w1))].Watches.Add(w1);
-        literalData[LiteralIdx(GetLiteralForWatch(w2))].Watches.Add(w2);
+        literalData.At(GetLiteralForWatch(w1)).Watches.Add(w1);
+        literalData.At(GetLiteralForWatch(w2)).Watches.Add(w2);
       }
       public bool IsUnit(){  
         //  i) clause is unit if it has size 1 and the only literal is unsatisfied   
         // ii) clause is unit if one of the watches points to a falsified literal
         //     and the other watch points to an unsatisfied literal
-        return  (Literals.Count == 1 && literalData[LiteralIdx(Literals[0])].Value == Valuation.UNSATISFIED)
+        return  (Literals.Count == 1 && literalData.At(Literals[0]).Value == Valuation.UNSATISFIED)
              || (Literals.Count >= 2 && (
                  (GetValue(w1!) == Valuation.UNSATISFIED && GetValue(w2!) == Valuation.FALSIFIED)
                   || (GetValue(w2!) == Valuation.UNSATISFIED && GetValue(w1!) == Valuation.FALSIFIED)
              ));
       }
       public bool IsFalsified(){
-        return (Literals.Count == 1 && literalData[LiteralIdx(Literals[0])].Value == Valuation.FALSIFIED)
+        return (Literals.Count == 1 && literalData.At(Literals[0]).Value == Valuation.FALSIFIED)
               || (Literals.Count >= 2 && (
                 GetValue(w1) == Valuation.FALSIFIED && GetValue(w2) == Valuation.FALSIFIED
               ));
       }
 
+      public int GetUnitLiteral(){
+        if(w1 is null){
+          return Literals[0];
+        }
+        else if(GetValue(w1) == Valuation.UNSATISFIED){
+          return GetLiteralForWatch(w1);
+        }
+        else{
+          if(GetValue(w2) != Valuation.UNSATISFIED){
+              throw new ArgumentException(nameof(w2));
+          }
+          return GetLiteralForWatch(w2);  
+        }
+      }
       public override string ToString(){
         return $"literals: {string.Join(' ',
-                            Literals.Select(l => l.ToString() + '/' + literalData[LiteralIdx(l)].Value.ToString()))
+                            Literals.Select(l => l.ToString() + '/' + literalData.At(l).Value.ToString()))
                             } unit: {IsUnit()} falsified: {IsFalsified()} watches: {(Literals.Count == 1 ? null : w1.LitPos.ToString() + ' ' + w2.LitPos.ToString())}";
       }
       public Watch GetOtherWatch(Watch w){
@@ -120,9 +79,7 @@ namespace Kresat.Representations {
         throw new ArgumentException(nameof(w));
       }
       public Valuation GetValue(int pos){
-        //Console.WriteLine($"accessing {pos} of array of size {Literals.Count}");
-        //Console.WriteLine($"accessing {LiteralIdx(Literals[pos])} of array of size {literalData.Count}");
-        return literalData[LiteralIdx(Literals[pos])].Value;
+        return literalData.At(Literals[pos]).Value;
       }
       public int GetLiteralForWatch(Watch w){
         return Literals[w.LitPos];
@@ -131,76 +88,17 @@ namespace Kresat.Representations {
         return GetValue(w.LitPos);
       }
     }
-    public void Backtrack(int decisionLevel){
-      HasContradiction = false;
-      unitClauses.Clear();
-      while(decisions.Count > 0 && decisions.Peek().DecisionLevel > decisionLevel){
-        Decision decision = decisions.Pop();
-        UndoLiteral(decision.Literal);
-      }
-      currDecisionLevel = decisionLevel;
-    }
-    public void UnitPropagation(){
-      while(unitClauses.Count > 0 && !HasContradiction){
-        unitPropSteps++;
-        Clause currClause = unitClauses.Pop();
-        if(!currClause.IsUnit()){
-          if(currClause.IsFalsified()){
-            HasContradiction = true;
-          }
-          continue;
-        }
-        if(currClause.w1 is null){
-          AssignLiteral(currClause.Literals[0]);
-        }
-        else if(currClause.GetValue(currClause.w1) == Valuation.UNSATISFIED){
-          AssignLiteral(currClause.GetLiteralForWatch(currClause.w1));
-        }
-        else{
-          if(currClause.GetValue(currClause.w2) != Valuation.UNSATISFIED){
-            throw new ArgumentException(nameof(currClause.w2));
-          }
-          AssignLiteral(currClause.GetLiteralForWatch(currClause.w2));
-        }
-      }      
-    }
-    private void AssignLiteral(int literal){
-      //Console.WriteLine($"assigning {literal}");
-      decisions.Push(new Decision { DecisionLevel = currDecisionLevel, Literal = literal });
-      UndecidedVars.Remove(Math.Abs(literal));
-      literalData[LiteralIdx(literal)].Satisfy();
-      literalData[LiteralIdx(-literal)].Falsify();
-      /*foreach(var clause in clauseData){
-        Console.WriteLine($"D: {clause}");
-      }*/
-      foreach(var watch in literalData[LiteralIdx(-literal)].Watches){
-        if(watch.clause.IsUnit()){
-          //Console.WriteLine($"found unit {watch.clause}");
-          unitClauses.Push(watch.clause);
-        }
-        if(watch.clause.IsFalsified()){
-          //Console.WriteLine($"found falsified");
-          HasContradiction = true;
-        }
-      }
-    }
-    private void UndoLiteral(int literal)
-    {
-      //Console.WriteLine($"undoing {literal}");
-      UndecidedVars.Add(Math.Abs(literal));
-      literalData[LiteralIdx(literal)].Unsatisfy();;
-      literalData[LiteralIdx(-literal)].Unsatisfy();
-    }
 
-    class Watch {
-      public Clause clause;
+  internal class Watch {
+      public WatchClause clause;
       public int LitPos {get; private set;} = 0;
-      public Watch(int LitPos, Clause clause){
+      public Watch(int LitPos, WatchClause clause){
         this.clause = clause;
         this.LitPos = LitPos;
       }
       bool CanBeWatched(int pos){
-        return clause.GetValue(pos) != Valuation.FALSIFIED && clause.GetOtherWatch(this).LitPos != pos;
+        return clause.GetValue(pos) != Valuation.FALSIFIED
+            && clause.GetOtherWatch(this).LitPos != pos;
       }
       public bool FindNext(){
         // returns false if there's no unwatched unsatisfied literal left in the clause
@@ -216,52 +114,46 @@ namespace Kresat.Representations {
         return false;
       }
     }
-    List<Clause> clauseData;
-    List<Literal> literalData;
+  internal class WatchLiteral : ILiteral<WatchLiteral>, ICreateFromLiteralData<WatchLiteral> {
+      public List<Watch> Watches = new();
+      public Valuation Value {get; private set;} = Valuation.UNSATISFIED;
+      public List<WatchLiteral> literalData { get; set; }
 
-    internal int ChooseDecisionLiteral()
-        {
-            return UndecidedVars.First();
-        }
-
-    public void DecideLiteral(int literal){
-      currDecisionLevel++;
-      AssignLiteral(literal);
-    }
-
-     public void UndoLastLiteral(){
-      Backtrack(currDecisionLevel-1);
-    }
-    public WatchedLiterals(CommonRepresentation cr){
-      literalData = new List<Literal>( new Literal[2*cr.LiteralCount]);
-      for(int i = 0; i < literalData.Count; i++){
-        literalData[i] = new Literal{literalData = literalData};
+      public WatchLiteral(List<WatchLiteral> literalData){
+        this.literalData = literalData;
       }
-      clauseData = new();
-      UndecidedVars = new HashSet<int>(Enumerable.Range(1, cr.LiteralCount));
-      foreach(var clause in cr.Clauses){
-        AddClause(clause);
+      public static WatchLiteral Create(List<WatchLiteral> literalData){
+        return new WatchLiteral(literalData);    
       }
-    }
-        private void AddClause(List<int> literals)
-        {
-          Clause clause = new(literals, literalData);
-          clauseData.Add(clause);
-          //Console.WriteLine($"add clause: {clause}");
-          if(clause.IsUnit()){
-            unitClauses.Push(clause);
-          }
+      public void Satisfy(){
+        Value = Valuation.SATISFIED;
+      }
+      public IEnumerable<IClause<WatchLiteral>> GetClauses(){
+        foreach(var watch in Watches){
+          yield return watch.clause;
         }
-        internal List<int> ConstructModel()
-        {
-          List<int> res = new List<int>();
-          for(int _var = 1; _var <= literalData.Count / 2; _var++){
-            if(literalData[LiteralIdx(_var)].Value == Valuation.UNSATISFIED){
-              throw new ArgumentException();
+      }
+      public void Falsify(){
+        Value = Valuation.FALSIFIED;
+        for(int i = 0; i < Watches.Count; i++){
+          int prev = Watches[i].LitPos;
+          if(Watches[i].FindNext()){
+            int newLit = Watches[i].clause.Literals[Watches[i].LitPos];
+            if(prev == Watches[i].LitPos){
+              throw new ArgumentException("aaa");
             }
-            res.Add(literalData[LiteralIdx(_var)].Value == Valuation.SATISFIED ? _var : -_var);
+            literalData.At(newLit).Watches.Add(Watches[i]);
+            Watches.RemoveInPlace(i);
+            i--;
           }
-          return res;
         }
+      }
+      public void Unsatisfy(){
+        Value = Valuation.UNSATISFIED;
+      }
+    }
+    internal class WatchedLiterals : UnitPropagationDS<WatchLiteral, WatchClause>
+    {
+        public WatchedLiterals(CommonRepresentation cr) : base(cr){}
     }
 }
